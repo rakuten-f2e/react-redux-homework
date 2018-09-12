77760689d9d72a0548765e4b258848a15b28550a
Hsieh <ss77995ss@gmail.com>
2018-09-12 13:54:55 +0800

    Add footer to the website

---
+++ src/client/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render(){
+        return (
+            <div className="Home-footer">
+                <hr />
+                <footer>
+                    <p>Contribute by Li-Hsuan Hsieh</p>
+                    <p>E-mail: <a href="mailto:ss77995ss@gmai.com">ss77995ss@gmail.com</a></p>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
