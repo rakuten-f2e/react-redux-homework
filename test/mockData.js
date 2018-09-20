b4a11c9698725c426b0d09ba9cd349d81bd6ed81
Hsieh <ss77995ss@gmail.com>
2018-09-20 11:10:54 +0800

    Seperate odd & even data in mockData

--- test/mockData.js
+++ test/mockData.js
@@ -1,26 +1,22 @@
+export const odd = {
+  index:361,
+  seq: 3,
+  status: "Open",
+  category: "cat5",
+  title: "title500",
+  owner: "Sean",
+  priority: "P4"
+}
+export const even = {
+  index: 34,
+  seq: 314,
+  status: "Close",
+  category: "cat1",
+  title: "title5",
+  owner: "Shawn",
+  priority: "P3"
+}
 export const mockData = [
-  {
-    seq: 3,
-    status: "Open",
-    category: "cat5",
-    title: "title500",
-    owner: "Sean",
-    priority: "P4"
-  },
-  {
-    seq: 314,
-    status: "Close",
-    category: "cat1",
-    title: "title5",
-    owner: "Shawn",
-    priority: "P3"
-  },
-  {
-    seq: 0,
-    status: "Open",
-    category: "cat15",
-    title: "title00",
-    owner: "Sean",
-    priority: "P41111"
-  }
+  odd,
+  even,
 ];
